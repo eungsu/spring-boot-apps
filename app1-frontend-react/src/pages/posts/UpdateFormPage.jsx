0d11f07b438eb5c16bbd1adfded2635a6345b466
이응수 <eungsu@me.com>
2025-08-04 16:41:44 +0900

    Update UpdateFormPage.jsx

--- app1-frontend-react/src/pages/posts/UpdateFormPage.jsx
+++ app1-frontend-react/src/pages/posts/UpdateFormPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchPostDetail } from '../../api/postApi';
+import { fetchPostDetail } from '../../api/posts';
 import PageContainer from '../../layouts/PageContainer';
 import PostUpdateForm from '../../components/posts/PostUpdateForm';
 
